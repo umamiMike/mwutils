e0b8a409fe662b15d2d2684d405965f10b164859
Michael Wilding <michaelwchastain@gmail.com>
2019-02-12 22:46:27 -0800

    adds variadic param for time

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 //mainly using for demo printout
-func PrintWait(s string, t int) {
+func PrintWait(s string, t ...float64) {
+	dur := 1.0
+	if len(t) > 0 {
+		dur = t[0]
+	}
 	fmt.Println(s)
-	time.Sleep(time.Duration(t) * time.Second)
+	time.Sleep(time.Duration(dur) * 1000 * time.Millisecond)
+
 }
